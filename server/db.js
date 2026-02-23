5795895da2e662eaee8065e51793650330e743a8
Dziuun <kamil.dziobekk@gmail.com>
2026-02-23 14:23:42 +0100

    experimenting with express

--- server/db.js
+++ server/db.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const uri = "";
+const uri =
+  "mongodb+srv://kamildziobekk_db_user:6xue9fBE4QV3rN2C@joepartycluster.4kxkdkf.mongodb.net/joeparty?appName=JoepartyCluster";
 
 export async function connectDB() {
   try {
     await mongoose.connect(uri);
     console.log("Connected with Mongoose");
+    return mongoose.connection;
   } catch (err) {
     console.error("DB connection error:", err);
+    throw err;
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,98 +1,35 @@
-import { createServer } from "http";
-import { connectDB, getCollection } from "./db.js";
-import { getRandomIndexNumber } from "./utils.js";
-import { NUMBER_OF_CATEGORIES, QUESTIONS_PER_CATEGORY } from "../config.js";
+import { connectDB } from "./db.js";
+import dns from "node:dns/promises";
+import express from "express";
 
-const PORT = process.env.PORT;
+const app = express();
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+let db;
 
-await connectDB();
-const collection = getCollection();
-
-const getRequiredQuestions = (req, res) => {
-  let body = "";
-
-  req.on("data", (chunk) => {
-    body += chunk.toString();
-  });
-  req.on("end", async () => {
-    const data = JSON.parse(body);
-    const reqCat = data.requestedCategories;
-    const reqQuestions = await selectQuestions(reqCat);
-
-    res.statusCode = 201;
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-
-    res.end(JSON.stringify(reqQuestions));
-  });
-};
-
-async function selectQuestions(reqCat) {
-  const questions = await collection.find({}).toArray();
-  let selCat = [];
-  let selQuestions = [];
-
-  if (reqCat.length > 6) selCat = randomizeCategories(reqCat);
-  else {
-    selCat = reqCat;
-  }
-
-  for (let i = 0; i < selCat.length; i++) {
-    // this goes over cats
-    const curCat = selCat[i];
-
-    for (let j = 1; j < 6; j++) {
-      const curCatQuest = questions.filter(
-        (q) => q.category === curCat && Number(q.questionValue) === j * 100,
-      );
-
-      selQuestions.push(curCatQuest[getRandomIndexNumber(curCatQuest.length)]);
-    }
-  }
-
-  return selQuestions;
-  // 1. Get 6 categories from provided categories 2. get 5 questions for each category 3. qustions need to be in values of 100 => 500
+async function connectplz() {
+  db = await connectDB();
 }
 
-function randomizeCategories(reqCat) {
-  let selCat = [];
+// async function getQuestions() {
+//   try {
+//     const questions = db.collection("questions");
+//     const allQuestions = await questions.find({}).toArray();
+//     console.log(allQuestions);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
 
-  do {
-    const r = getRandomIndexNumber(reqCat.length);
-    const curCat = reqCat[r];
+await connectplz();
+// await getQuestions();
 
-    if (!selCat.includes(curCat)) selCat.push(curCat);
-  } while (selCat.length < 6);
-  console.log(selCat);
-  return selCat;
-}
+app.get("/api/categories", async (req, res) => {
+  console.log("I WORK");
+  const cats = await db.collection("questions").distinct("category");
 
-const server = createServer(async (req, res) => {
-  const questions = await collection.find({}).toArray();
-  if (req.method === "OPTIONS") {
-    res.writeHead(204, {
-      "Access-Control-Allow-Origin": "http://localhost:5173",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
-    });
-    res.end();
-    return;
-  } else if (req.url === "/api/questions" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.write(JSON.stringify(questions));
-    res.end();
-  } else if (req.url === "/api/questions/categories" && req.method === "GET") {
-    const categories = [...new Set(questions.map((q) => q.category))];
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.write(JSON.stringify(categories));
-    res.end();
-  } else if (req.url === "/api/startGame" && req.method === "POST") {
-    getRequiredQuestions(req, res);
-  }
+  console.log(cats);
+  res.json(cats);
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+app.listen(8000, () => console.log("server is listening on the port 8000"));
+// 1) get categories only, get questions from submited catgory
